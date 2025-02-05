33dba3f50e35910cbf24ea15d3bccd22d7efe441
MousaThe14 <MousaThe14@gmail.com>
2025-02-04 23:24:47 -0500

    Updated data and did some work on the least popular Pokemon

--- RegionPopularity.R
+++ RegionPopularity.R
@@ -25,8 +25,27 @@ TopGen2 <- average_ratings %>% filter(Generation == "2") %>% slice_max(Popularit
 TopGen3 <- average_ratings %>% filter(Generation == "3") %>% slice_max(Popularity, n = 10)
 TopGen4 <- average_ratings %>% filter(Generation == "4") %>% slice_max(Popularity, n = 10)
 TopGen5 <- average_ratings %>% filter(Generation == "5") %>% slice_max(Popularity, n = 10)
-TopGen6 <- average_ratings %>% filter(Region == "Kalos") %>% slice_max(Popularity, n = 20)
+TopGen6 <- average_ratings %>% filter(Region == "Kalos") %>% slice_max(Popularity, n = 10)
 TopGen7 <- average_ratings %>% filter(Generation == "7") %>% slice_max(Popularity, n = 10)
 TopGen8 <- average_ratings %>% filter(Region == "Galar") %>% slice_max(Popularity, n = 10)
 TopGen9 <- average_ratings %>% filter(Generation == "9") %>% slice_max(Popularity, n = 10) 
 
+BotGen1 <- average_ratings %>% filter(Generation == "1") %>% slice_min(Popularity, n = 1)
+BotGen2 <- average_ratings %>% filter(Generation == "2") %>% slice_min(Popularity, n = 1)
+BotGen3 <- average_ratings %>% filter(Generation == "3") %>% slice_min(Popularity, n = 1)
+BotGen4 <- average_ratings %>% filter(Generation == "4") %>% slice_min(Popularity, n = 1)
+BotGen5 <- average_ratings %>% filter(Generation == "5") %>% slice_min(Popularity, n = 1)
+BotGen6 <- average_ratings %>% filter(Generation == "6") %>% slice_min(Popularity, n = 1)
+BotGen7 <- average_ratings %>% filter(Generation == "7") %>% slice_min(Popularity, n = 1)
+BotGen8 <- average_ratings %>% filter(Generation == "8") %>% slice_min(Popularity, n = 1)
+BotGen9 <- average_ratings %>% filter(Generation == "9") %>% slice_min(Popularity, n = 1)
+
+BottomGen <- BotGen1 %>%
+  rbind(BotGen2) %>%
+  rbind(BotGen3) %>%
+  rbind(BotGen4) %>%
+  rbind(BotGen5) %>%
+  rbind(BotGen6) %>%
+  rbind(BotGen7) %>%
+  rbind(BotGen8) %>%
+  rbind(BotGen9) 
--- TheMostUnattractivePokemonAccordingToYou.R
+++ TheMostUnattractivePokemonAccordingToYou.R
@@ -16,38 +16,55 @@ library(splitstackshape)
 
 average_ratings <- read.csv("average-ratings_enriched.csv")
 raw_ratings <- read.csv("all-ratings_enriched.csv")
-globalAverage <- read.csv("GlobalAverages.csv")
+globalAverageTraits <- read.csv("GlobalAverages.csv")
+globalAverageGenerations <- read.csv("GlobalAverage-Gen.csv")
 
+globalAverageTypes <- read.csv( "GlobalAverages-Type.csv")
 # Global Averages
-# Popularity  3.535
+# Popularity  3.533
 # Beauty      3.019
+# Ugliest Type    - Poison - 2.669
+# Ugliest Gen     -  Gen 8 - 2.878
 # Cuteness    3.019
+# Least Cute Type - Steel - 2.488
+# Least Cute Gen  - 8 - 2.801
+# Most Popular Gen - Gen 2 - 3.682
+# Least Popular Gen - Gen 9 - 3.356 
+# Most Popular Type - Ghost - 3.810
+# Least Popular Type - Normal - 3.402
 
 
 ugliest30 <- average_ratings %>% slice_min(Beauty, n = 30)
 uncute30 <- average_ratings %>% slice_min(Cuteness, n = 30)
 
 UglyUncute <- full_join(ugliest30, uncute30)
+ 
+# ugliest31 <- average_ratings %>% slice_min(Beauty, n = 31)
+# uncute31 <- average_ratings %>% slice_min(Cuteness, n = 31)
+# 
+# UglyUncute2 <- full_join(ugliest31, uncute31)
 
-ugliest31 <- average_ratings %>% slice_min(Beauty, n = 31)
-uncute31 <- average_ratings %>% slice_min(Cuteness, n = 31)
-
-UglyUncute2 <- full_join(ugliest31, uncute31)
-
-UglyUncuteCommon <- inner_join(ugliest31, uncute31)
+UglyUncuteCommon <- inner_join(ugliest30, uncute30)
 
 LeastPopular50 <- average_ratings %>% slice_min(Popularity, n = 50)
 
-UnpopularUnattractive <- inner_join(UglyUncute2, LeastPopular50)
+UnpopularUnattractive <- inner_join(UglyUncute, LeastPopular50)
+
+LeastPopLeastCute <- inner_join(LeastPopular50, uncute30)
+LeastPopUgly <- inner_join(LeastPopular50, ugliest30)
+
+UncuteUglyUnpopular <- full_join(LeastPopUgly, LeastPopLeastCute)
+
 
 
 averages_beauty_cute_ratio <- average_ratings %>% mutate(BeautyOverCute = Beauty/Cuteness)
 
 
 
-
-
-
+JynxRatings5 <- raw_ratings %>% filter(PokemonName == "Jynx" & Popularity == 5)
+JynxRatings4 <- raw_ratings %>% filter(PokemonName == "Jynx" & Popularity == 4)
+nrow(JynxRatings4)
+nrow(JynxRatings5)
   
 ggplot(average_ratings, aes(x = Cuteness, y = Beauty)) +
   geom_point() +
